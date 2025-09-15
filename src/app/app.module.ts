830a97359b4009f9b1f6748ee6ce0af3ff126186
rolyan66@gmail.com <rolyan66@gmail.com>
2025-09-15 11:59:13 -0400

    Se realizo la creacion de la estructura del componente de bitly

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ShortUrlComponent } from './components/short-url/short-url.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ShortUrlComponent,
+    SpinnerComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
